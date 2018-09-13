6debd2e8c098a51e3ac56a6209e06b52562d6e09
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-12 19:31:39 -0500

    added items to transaction list and looping over

--- TransactionTuples/Program.cs
+++ TransactionTuples/Program.cs
@@ -9,6 +9,21 @@ namespace TransactionTuples
         {
             List<(string product, double amount, int quantity)> transactions = new List<(string, double, int)>();
 
+            transactions.Add(("CL-107", 19.99, 500));
+            transactions.Add(("CL-108", 29.99, 300));
+            transactions.Add(("CL-109", 39.99, 275));
+            transactions.Add(("CL-110", 49.99, 250));
+            transactions.Add(("L-220", 1.99, 7000));
+
+            int sales = 0;
+            double revenue = 0;
+
+            foreach ((string product, double amount, int quantity) in transactions)
+            {
+                sales += quantity;
+                revenue += amount * quantity;
+            }
+
             Console.ReadLine();
 
         }
